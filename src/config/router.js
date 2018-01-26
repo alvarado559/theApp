c7412a129e5e27a101e6be3008316e2f2b1c8c1b
Albert Alvarado <alalvarado@csumb.edu>
2018-01-25 17:50:50 -0800

    added icons to tabs

--- src/config/router.js
+++ src/config/router.js
@@ -17,19 +17,35 @@ export const Tabs = TabNavigator({
     screen:Menu,
     navigationOptions: {
       tabBarLabel: 'Menu',
-      tabBarIcon:({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
+      tabBarIcon:({ tintColor }) => <Icon type='foundation' name="list" size={35} color={tintColor} />
     },
   },
   News:{
     screen:News,
+    navigationOptions: {
+      tabBarLabel: 'News',
+      tabBarIcon:({ tintColor }) => <Icon  name='feedback' color='#517fa4' />
+    },
   },
   Staff:{
     screen:Staff,
+    navigationOptions: {
+      tabBarLabel: 'Staff',
+      tabBarIcon:({ tintColor }) => <Icon  name='people' color='#517fa4' />
+    },
   },
   Events:{
     screen:Events,
+    navigationOptions: {
+      tabBarLabel: 'Events',
+      tabBarIcon:({ tintColor }) => <Icon  name='assignment' color='#517fa4' />
+    },
   },
   Laws:{
     screen:Laws,
+    navigationOptions: {
+      tabBarLabel: 'Laws',
+      tabBarIcon:({ tintColor }) => <Icon  name='visibility' color='#517fa4' />
+    },
   }
 });
